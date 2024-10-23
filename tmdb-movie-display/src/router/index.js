462fad0fcef312debab6ab582cea9af652d35968
nikoUCN <10478737@ucn.dk>
2024-10-23 22:14:17 +0200

    added scrollToTop function

--- tmdb-movie-display/src/router/index.js
+++ tmdb-movie-display/src/router/index.js
@@ -2,18 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import MoviePage from '../components/MoviePage.vue';
 import FavoritesPage from '../components/FavoritesPage.vue';
+import DetailsPage from '../components/DetailsPage.vue';
+import SearchPage from '../components/SearchPage.vue';
 
 //defining routes 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/movies', component: MoviePage },
-    { path: '/favorites', component: FavoritesPage }
+    { path: '/favorites', component: FavoritesPage },
+    { path: '/details/:id', component: DetailsPage},
+    { path: '/search', component: SearchPage}
 ];
 
 //new instance of VueRouter with the routes defined earlier
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition){ 
+        if(savedPosition){
+            return savedPosition;
+        }
+        else{
+            return { top: 0 };
+        
+        }
+    }
 });
 
 export default router;
