5825bb4066ec2ae6e2804d870017be6c05daa54a
nikoUCN <10478737@ucn.dk>
2024-10-25 15:10:41 +0200

    made favorites page
    Now you can see your favorite movies in a session

--- tmdb-movie-display/src/components/FavoritesPage.vue
+++ tmdb-movie-display/src/components/FavoritesPage.vue
@@ -1,16 +1,103 @@
 <template>
-    <div>
-      <h1>Favorites Page</h1>
-      <!-- Favorites page content -->
+  <div class="favorite-header-container">
+    <h1>Favorites</h1>
+  </div>
+  <div class="favorite-movie-container">
+    <div v-if="favoriteMovies.length" class="favorite-movie-list">
+      <div v-for="movie in favoriteMovies" :key="movie.id" class="favorite-movie-box">
+        <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="Movie poster" />
+        <div class="movie-title">
+          <a>{{ movie.title }}</a>
+        </div>
+      </div>
     </div>
-  </template>
-  
+    <div v-else class="no-movies-text">
+      <p>No favorite movies yet</p>
+    </div>
+  </div>
+    
+</template>
+
+
   <script>
   export default {
-    name: 'FavoritesPage'
+    name: 'FavoritesPage',
+    data(){
+      return{
+        favoriteMovies: [] // Array to store the favorite movies
+      };
+    },
+    async created(){
+      // Fetching the favorite movies from local storage
+      const favoriteMovies = JSON.parse(sessionStorage.getItem('favorites')) || [];
+      this.favoriteMovies = favoriteMovies; // Setting the favorite movies
+    }
   }
   </script>
   
-  <style>
-  /* Your styles here */
+  <style scoped>
+  .favorite-header-container {
+    display: flex;
+    justify-content: flex-start;
+    padding-top: calc(var(--navbar-height) + 20px);
+    padding-left: 30px;
+    
+  }
+
+  .favorite-header-container h1{
+    font-size: 40px;
+    color: white;
+    font-weight: bold;
+  }
+
+  .favorite-movie-container {
+    display: flex;
+    justify-content: center;
+  }
+  
+
+  .favorite-movie-list {
+      display: grid;
+      grid-template-columns: repeat(3, minmax(200px, 1fr));
+      padding: 20px;
+      gap: 20px;
+  }
+
+  .favorite-movie-box{
+    transition: transform 0.5s ease;
+  }
+
+  .favorite-movie-box:hover{
+    transform: scale(1.02);
+  }
+
+  .favorite-movie-box img{
+    width: 100%;
+    height: auto;
+    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+  }
+  
+  .movie-title a{
+      display: flex;
+      justify-content: flex-start;
+      font-size: 20px;
+      font-weight: bold;
+      color: white;
+      text-decoration: none;
+      margin: 10px 0;
+      cursor: pointer;
+  }
+
+  .no-movies-text{
+    display: flex;
+    justify-content: center;
+    padding-top: 150px;
+    height: 100vh;
+  }
+
+  .no-movies-text p{
+    font-size: 20px;
+    color: white;
+  }
   </style>
