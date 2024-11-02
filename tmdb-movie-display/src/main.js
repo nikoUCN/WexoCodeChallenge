6171dd841c71ee55ef0cd8b2d0900954c613545e
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-11-02 12:58:49 +0100

    added comments to  main

--- tmdb-movie-display/src/main.js
+++ tmdb-movie-display/src/main.js
@@ -4,8 +4,14 @@ import router from './router';
 import { FontAwesomeIcon } from './plugins/fontAwesome';
 import './styles/global.css';
 
+// Create the app instance
 const app = createApp(App);
 
+// Register the font-awesome icon component
 app.component('font-awesome-icon', FontAwesomeIcon);
+
+// Register the router instance
 app.use(router);
+
+// Mount the app instance to the DOM
 app.mount('#app');
