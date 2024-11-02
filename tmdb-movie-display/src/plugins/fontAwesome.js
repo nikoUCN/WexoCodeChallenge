19c22025946c57432aadfbd7a67401dff8a6afdc
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-11-02 12:59:11 +0100

    comments

--- tmdb-movie-display/src/plugins/fontAwesome.js
+++ tmdb-movie-display/src/plugins/fontAwesome.js
@@ -1,3 +1,5 @@
+// FontAwesome package for Vue.js to display the icons in the application.
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBookmark as farBookmark } from '@fortawesome/free-regular-svg-icons';
 import { faBookmark as fasBookmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
